a60bfcd9495debfa1920eb4f8ed8c270f18ed290
Dax Sherwood <daxsherwood@localhost>
2024-05-09 20:42:28 -0600

    leaderboard changes

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -19,44 +19,56 @@ public class Leaderboard extends JPanel{
 	
 	private HighScoreDatabase database;
 	
-	
+	/**
+	 * A Constructor to set up the main JPanel
+	 * @param parent
+	 */
 	public Leaderboard(JFrame parent) {
 		database = new HighScoreDatabase();
 		this.parent = parent;
         this.setSize(600, 800);
         parent.setLayout(new BorderLayout()); 
         
-        leaderboardPanel = new JPanel();
-        leaderboardPanel.setLayout(new BorderLayout());
+        leaderboardPanel = new JPanel(new BorderLayout());
+        JPanel center = new JPanel(new GridLayout(2,0));
         
-        JLabel header = new JLabel("High Scores");
+        JLabel header = new JLabel("High Scores!", SwingConstants.CENTER);
         header.setForeground(Color.BLUE);
         header.setFont(new Font("Serif", Font.BOLD, 34));
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+		header.setVerticalAlignment(SwingConstants.BOTTOM);
         
-        leaderboardPanel.add(header, BorderLayout.NORTH);
-        leaderboardPanel.add(leaderboardLabel(), BorderLayout.CENTER);
-        leaderboardPanel.setBackground(Color.BLACK);
+        center.add(header);
+        center.add(leaderboardLabel());
+        center.setBackground(Color.BLACK);
+        
+        
         JPanel buttons = new JPanel(new GridLayout());
         buttons.setBackground(Color.BLACK);
 
         JButton playAgain = new JButton("Play Again");
-        JButton exit = new JButton("Exit");
+        JButton exit = new JButton("Main Menu");
         
         buttons.add(playAgain);
         buttons.add(exit);
-        leaderboardPanel.add(buttons,BorderLayout.SOUTH);
         
-		parent.add(leaderboardPanel, BorderLayout.CENTER);
+        leaderboardPanel.add(buttons,BorderLayout.SOUTH);
+        leaderboardPanel.setBackground(Color.BLACK);
+        leaderboardPanel.add(center, BorderLayout.CENTER);
 		
 		
-		parent.setBackground(Color.WHITE);
+        parent.add(leaderboardPanel, BorderLayout.CENTER);
 		parent.setVisible(true);
-		
 	}
 	
+	/**
+	 * This method grabs the high scores from the data base
+	 * @return a jlabel containing the scores
+	 */
 	private JLabel leaderboardLabel() {
-		theScores = new JLabel();
+		
 		String scoreString = "<html>";
+		theScores = new JLabel(scoreString, SwingConstants.CENTER);
 		ArrayList<String> scores = database.getTopNScores(10);
 		for(int i = 0; i < scores.size(); i++) {
 			scoreString += scores.get(i) + "<br/>";
@@ -65,8 +77,10 @@ public class Leaderboard extends JPanel{
 		theScores.setText(scoreString);
 		theScores.setBackground(Color.BLUE);
 		theScores.setForeground(Color.YELLOW);
+		theScores.setFont(new Font("Serif", Font.BOLD, 24));
+		theScores.setHorizontalAlignment(SwingConstants.CENTER);
+		theScores.setVerticalAlignment(SwingConstants.NORTH);
 		return theScores;
-		
 	}
 	
 	
